c1409ac76b05ed274473d540af5ffe9e5d0a86d2
Yanece <k.yanemichel@gmail.com>
2025-10-23 12:48:25 +0200

    feat: adding adjacent table and starting node object

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from logging import root
 import pandas
 
 csv_content = pandas.read_csv("parcours_explorateurs.csv")
 print(csv_content.head(100))
+
+adjacent_table = {}
+adjacent_table = {
+    row["noeud_amont"]: row["noeud_aval"] for _, row in csv_content.iterrows()
+}
